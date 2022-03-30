cb80958976f2aae3a77c5dfa7588db386b850635
surgudla <surgudla@iu.edu>
2022-03-30 18:35:20 -0400

    removed HTRC sub module

--- HTRC-DataCapsules/backend/checknginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-if [[ -s $1/restart_nginx_out ]]; then 
-	echo "Nginx install issue in $1."
- fi
--- HTRC-DataCapsules/backend/uploads/jupyter-notebooks/JupyterNotebooks/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GIT_REPO_PATH="/home/dcuser/HTRC-JupyterNotebooks"
-
-cd $GIT_REPO_PATH
-export BROWSER=google-chrome
-source activate nb_env
-jupyter notebook
